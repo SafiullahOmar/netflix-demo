3ebf8732a9bce97b3cb174c6b7d87f65ed3d1b01
safiullah omar <omar.safiullah@gmail.com>
2024-01-08 23:52:34 +0100

    some more work

--- src/Row.jsx
+++ src/Row.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import axios from './axios';
+import { useState } from 'react';
 
 function Row({title},fetchUrl) {
     const [movies,setMovies]=useState([]);
@@ -9,11 +10,19 @@ function Row({title},fetchUrl) {
             console.log(request);
             return request;
         }
-        
-    },[]);
+         
+    },fetchUrl);
   return (
     <div>
         <h2>{title}</h2>
+        <div className='row_posters'>
+            {
+                movies.map(movie=>(
+                    
+                    <img src={movie.poster_path} alt={movie.poster_path}/>
+                ))
+            }
+        </div>
 
         {/* title */}
     </div>
