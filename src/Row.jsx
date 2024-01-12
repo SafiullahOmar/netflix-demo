cc2bb2d718e8cecbc2741f17675d1ef2845eb9dd
safiullah omar <omar.safiullah@gmail.com>
2024-01-12 11:24:52 +0100

    getting the image from netflix api

--- src/Row.jsx
+++ src/Row.jsx
@@ -2,30 +2,45 @@ import React, { useEffect } from 'react'
 import axios from './axios';
 import { useState } from 'react';
 
-function Row({title},fetchUrl) {
+const baseBaseUrl="https://image.tmdb.org/t/p/original";
+
+function Row({title,fetchUrl }) {
     const [movies,setMovies]=useState([]);
+
     useEffect(()=>{
-        async function fetchData(){
-            const request=await axios.get(fetchUrl);
-            console.log(request);
-            return request;
-        }
-         
-    },fetchUrl);
-  return (
-    <div>
-        <h2>{title}</h2>
-        <div className='row_posters'>
-            {
-                movies.map(movie=>(
-                    
-                    <img src={movie.poster_path} alt={movie.poster_path}/>
-                ))
+        console.log(axios);
+
+        async function fetchData() {
+            try {
+                const request = await axios.get(fetchUrl);
+                console.log('Request:', request);
+                setMovies(request.data.results);
+                return request;
+            } catch (error) {
+                console.error('Error fetching data:', error);
+                // Handle errors as needed
             }
+        }
+
+        fetchData();
+         
+    },[fetchUrl]);
+
+  return (
+    
+        <div className='row'>
+            <h2>{title}</h2>
+            <div className='row_posters'>
+        
+                {movies.map(movie => (
+                    
+                    <img key={movie.id} src={`${baseBaseUrl}${movie.backdrop_path}`} alt={`${baseBaseUrl}${movie.backdrop_path}`} />
+                ))}
+            </div>
         </div>
 
-        {/* title */}
-    </div>
+        
+    
   )
 }
 
