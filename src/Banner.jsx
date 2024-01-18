b561ce43d9da2e427c61b355b5b3116d584b146f
safiullah omar <omar.safiullah@gmail.com>
2024-01-18 23:53:21 +0100

    added button header and linear gradient desing

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from './axios';
 import requests from './requests';
+import  bannercss from  './baner.css'
 function Banner() {
   const [movie,setMovie]=useState([]);
   useEffect(()=>{
@@ -11,8 +12,26 @@ function Banner() {
     }
     fetchData();
   },[]);
+
+  function truncate(str,n){
+    return str?.length>n ? str.substr(0,n-1)+"...":str;
+  }
   return (
-    <header>
+    <header className='banner' style={{backgroundSize:"cover",backgroundPosition:"center center",backgroundImage:`url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`}}>
+      <div className="banner__contents">
+        <h1 className='banner__title'>
+          {movie?.title||movie?.name||movie?.originale_name}
+        </h1>
+
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h1 className="banner__description">
+          {truncate(movie?.overview,150)}
+        </h1>
+      </div>
+      <div className="banner--fadeBottom"></div>
 
     </header>
   )
