4fc2cbe3bbc624e4ca6bd0aab0a69520f155c24a
safiullah omar <omar.safiullah@gmail.com>
2024-01-21 12:31:03 +0100

    adding navbar and setting the css and style

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import img from "./netlfix_img.jpeg";
 import usr_img from  "./usr.png";
+import  "./nav.css";
 
 function Nav() {
+
+  const [show,handleShow]=useState(false);
+  useEffect(()=>{
+    window.addEventListener("scroll",()=>{
+      if(window.scrollY>100){
+          handleShow(true);
+      }else{
+          handleShow(false);
+      }
+      return ()=>{
+        window.removeEventListener("scroll");
+      }
+    });
+  },[]);
   return (
-    <div className='nav'>
+    <div className={`nav ${show && "nav_black"}`}>
         <img src={img}  className="nav_logo" alt="netflix"/>
-        <img src={usr_img}  className="nav_avatar" alt="avatar"/>
+        <img src={usr_img}  className="nav_user" alt="avatar"/>
     </div>
   )
 }
